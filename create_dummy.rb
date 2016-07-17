88af07b5acfa6c52fc6ab8d12a02bfb8664f86cd
Anton Davydov <antondavydov.o@gmail.com>
2016-07-17 16:42:51 +0300

    Add index page for awesome hanami site

--- create_dummy.rb
+++
@@ -1,7 +0,0 @@
-# The goal is to make github repo as ruby category
-readme_line_count = File.foreach("README.md").inject(0) {|c, line| c+1}
-fp = File.open("dummy.rb", "w")
-
-(1..readme_line_count*2).each { |num| fp.puts("puts \"#{num}\"") }
-
-fp.close
